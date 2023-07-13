122be15d1a738faee18d08575fb252d95686344e
Stelless <47225505+Stelless@users.noreply.github.com>
2023-07-13 12:36:44 -0700

    Added stats to check amount spent, amount won, and payback rate

--- SlotsProto/SlotMachine.cpp
+++ SlotsProto/SlotMachine.cpp
@@ -5,7 +5,7 @@
 SlotMachine::SlotMachine() : SlotMachine::SlotMachine(DefaultReelCount) {}
 
 
-SlotMachine::SlotMachine(int total_reels) {
+SlotMachine::SlotMachine(int total_reels) : price_per_payline(Default_Price_Per_PayLine), credits(Default_Starting_Credits) {
    for (int reelindex = 0; reelindex < total_reels; reelindex++) {
       reels.push_back(Reel{});
       reelStops.push_back(0);
@@ -20,7 +20,9 @@ void SlotMachine::printViewingWindow()
    std::cout << "---CURRENT VIEWING WINDOW---" << std::endl;
    reelview.printReelView(reels, reelStops);
    std::cout << "---END VIEWING WINDOW---" << std::endl;
-   reelview.printPayLineCombos();
+   std::cout << "AVAILABLE CREDITS: " << credits << std::endl;
+   std::cout << "Current PayBackRate: " << pay_back_rate << std::endl;
+   //reelview.printPayLineCombos();
 
 }
 
@@ -46,15 +48,25 @@ void SlotMachine::checkWin()
 {
    int jackpot_total = evaluator.checkPayLineCombinations(reelview.getPayLineCombos());
    if (jackpot_total > 0) {
-      std::cout << "\n \n \n \n YOU WON!!! \n CREDITS EARNED: " << jackpot_total << std::endl;
+      credits += jackpot_total;
+      credits_won += jackpot_total;
    }
 }
 
 void SlotMachine::playRound()
 {
+   //Give them 3 credits to start. Each bet will be 3 credits. 1 credits on each payline
+   //The math should be a 1:100 chance of winning as there are 1 jackpots each with a 1/1000 chance
+   //To start this means that each jackpot should pay back 103 credits. This should be 100% payback?
+   // This is what I need to test next.
+   int cost = price_per_payline * reelview.getPayLineCombos().size();
+   credits -= cost;
+   credits_used += cost;
+
    spinReels();
    printViewingWindow();
    checkWin();
+   upDatePayPackRate();
 }
 
 void SlotMachine::printReelInfo()
@@ -66,4 +78,9 @@ void SlotMachine::printReelInfo()
       }
       std::cout << std::endl;
    }
+}
+
+void SlotMachine::upDatePayPackRate()
+{
+   pay_back_rate = credits_won / credits_used;
 }
--- SlotsProto/SlotMachine.h
+++ SlotsProto/SlotMachine.h
@@ -18,12 +18,22 @@ public:
 private:
 
    static constexpr int DefaultReelCount = 3;
+   static constexpr int Default_Price_Per_PayLine = 1;
+   static constexpr int Default_Starting_Credits = 3;
    std::vector<Reel> reels;
    std::vector<unsigned int> reelStops;
    ReelView reelview;
    CombinationEvaluator evaluator;
 
+   int credits_used{ 0 };
+   int credits_won{ 0 };
+   double pay_back_rate{ 0 };
+
+   int price_per_payline;
+   int credits;
+
 
    void printReelInfo();
+   void upDatePayPackRate();
 };
 
